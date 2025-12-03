dd8bfd1972199c7a1f96f40fd084cec7e7005167
shivgodi7 <shivgodi7@gmail.com>
2025-12-03 09:22:59 +0530

    Updates

---
+++ App.js
@@ -0,0 +1,19 @@
+import Navbar from "../Navbar";
+import Home from "../Home";
+import About from "../About";
+import Projects from "../Projects";
+import Contact from "../Contact";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
